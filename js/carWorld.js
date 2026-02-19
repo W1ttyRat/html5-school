13e097fee28d53bbb20468d7a7228b07f1b9598c
Tanel Metshein <tanemets@tlu.ee>
2026-02-19 19:36:02 +0200

    Enhance carWorld and clock pages with new interactive controls for moon and earth rotation

--- js/carWorld.js
+++ js/carWorld.js
@@ -3,11 +3,18 @@ const carWheel = document.getElementsByClassName("car-wheel");
 const moon = document.getElementById("moon");
 const moonBox = document.getElementById("moon-box");
 let rotationEarth = 0;
-let rotationMoon = 0;
 let rotation = 0;
+let rotationMoon = 0;
+
+let rotationEarthDegree = 0.125;
+let rotationMoonDegree = 0.125;
+
+let isMoonReversed = false;
+let isEarthReversed = true;
+
 
 function rotateEarth() {
-    rotationEarth -= 0.125;
+    rotationEarth -= rotationEarthDegree;
     earth.style.transform = `rotate(${rotationEarth}deg)`;
 }
 
@@ -19,11 +26,80 @@ function rotateCarWheel() {
 }
 
 function rotateMoon() {
-    rotationMoon += 0.125;
+    rotationMoon += rotationMoonDegree;
     moonBox.style.transform = `rotate(${rotationMoon}deg)`;
     moon.style.transform = `rotate(${-rotationMoon * 0.5}deg)`;
 }
 
+function moveMoonFaster() {
+    rotationMoonDegree += 0.125;
+}
+
+function moveMoonSlower() {
+    rotationMoonDegree -= 0.125;
+}
+
+
+
+function turnMoonAround() {
+    rotationMoonDegree = -rotationMoonDegree;
+    isMoonReversed = !isMoonReversed;
+    
+    // Swap the button functionality
+    const fasterBtn = document.getElementById("fasterMoon");
+    const slowerBtn = document.getElementById("slowerMoon");
+    
+    if (isMoonReversed) {
+        fasterBtn.removeEventListener("click", moveMoonFaster);
+        slowerBtn.removeEventListener("click", moveMoonSlower);
+        fasterBtn.addEventListener("click", moveMoonSlower);
+        slowerBtn.addEventListener("click", moveMoonFaster);
+    } else {
+        fasterBtn.removeEventListener("click", moveMoonSlower);
+        slowerBtn.removeEventListener("click", moveMoonFaster);
+        fasterBtn.addEventListener("click", moveMoonFaster);
+        slowerBtn.addEventListener("click", moveMoonSlower);
+    }
+}
+
+function moveEarthFaster() {
+    rotationEarthDegree += 0.125;
+}
+
+function moveEarthSlower() {
+    rotationEarthDegree -= 0.125;
+}
+
+function turnEarthAround() {
+    rotationEarthDegree = -rotationEarthDegree;
+    isEarthReversed = !isEarthReversed;
+    
+    // Swap the button functionality
+    const fasterBtn = document.getElementById("fasterEarth");
+    const slowerBtn = document.getElementById("slowerEarth");
+    
+    if (isEarthReversed) {
+        fasterBtn.removeEventListener("click", moveEarthFaster);
+        slowerBtn.removeEventListener("click", moveEarthSlower);
+        fasterBtn.addEventListener("click", moveEarthSlower);
+        slowerBtn.addEventListener("click", moveEarthFaster);
+    } else {
+        fasterBtn.removeEventListener("click", moveEarthSlower);
+        slowerBtn.removeEventListener("click", moveEarthFaster);
+        fasterBtn.addEventListener("click", moveEarthFaster);
+        slowerBtn.addEventListener("click", moveEarthSlower);
+    }
+}
+
+
 setInterval(rotateEarth, 10);
 setInterval(rotateCarWheel, 10);
 setInterval(rotateMoon, 10);
+
+document.getElementById("fasterMoon").addEventListener("click", moveMoonFaster);
+document.getElementById("slowerMoon").addEventListener("click", moveMoonSlower);
+document.getElementById("rotateMoon").addEventListener("click", turnMoonAround);
+
+document.getElementById("fasterEarth").addEventListener("click", moveEarthFaster);
+document.getElementById("slowerEarth").addEventListener("click", moveEarthSlower);
+document.getElementById("rotateEarth").addEventListener("click", turnEarthAround);
