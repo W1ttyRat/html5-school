14a0e13364e30e17b8fac412292335078da71ef0
Tanel Metshein <tanemets@tlu.ee>
2026-02-19 13:59:37 +0200

    Refactor code structure for improved readability and maintainability

--- js/carWorld.js
+++ js/carWorld.js
@@ -1,12 +1,15 @@
 const earth = document.getElementById("earth");
-const car = document.getElementById("car");
+const carWheel = document.getElementsByClassName("car-wheel");
 let rotation = 0;
 
-function rotateEarth() {
+function rotate() {
     rotation -= 0.25;
     earth.style.transform = `rotate(${rotation}deg)`;
+    for (let i = 0; i < carWheel.length; i++) {
+        carWheel[i].style.transform = `rotate(${-rotation * 4}deg)`;
+    }
 }
 
 
 
-setInterval(rotateEarth, 10);
+setInterval(rotate, 10);
