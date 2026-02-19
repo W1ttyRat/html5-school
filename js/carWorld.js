9801372dadb58d5ae12057a77f441b358b844517
Tanel Metshein <tanemets@tlu.ee>
2026-02-19 15:08:20 +0200

    Add moon image and implement rotation functionality

--- js/carWorld.js
+++ js/carWorld.js
@@ -1,15 +1,29 @@
 const earth = document.getElementById("earth");
 const carWheel = document.getElementsByClassName("car-wheel");
+const moon = document.getElementById("moon");
+const moonBox = document.getElementById("moon-box");
+let rotationEarth = 0;
+let rotationMoon = 0;
 let rotation = 0;
 
-function rotate() {
-    rotation -= 0.25;
-    earth.style.transform = `rotate(${rotation}deg)`;
+function rotateEarth() {
+    rotationEarth -= 0.125;
+    earth.style.transform = `rotate(${rotationEarth}deg)`;
+}
+
+function rotateCarWheel() {
+    rotation += 0.25;
     for (let i = 0; i < carWheel.length; i++) {
         carWheel[i].style.transform = `rotate(${-rotation * 4}deg)`;
     }
 }
 
+function rotateMoon() {
+    rotationMoon += 0.125;
+    moonBox.style.transform = `rotate(${rotationMoon}deg)`;
+    moon.style.transform = `rotate(${-rotationMoon * 0.5}deg)`;
+}
 
-
-setInterval(rotate, 10);
+setInterval(rotateEarth, 10);
+setInterval(rotateCarWheel, 10);
+setInterval(rotateMoon, 10);
